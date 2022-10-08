c46287d1cf1b7f107ae4cfad668f53cbded605d5
kcire <stevperdomo2017@gmail.com>
2022-10-07 19:31:21 -0500

    Change CV.pdf local for Google Drive

---
+++ main.js
@@ -0,0 +1,8 @@
+const btnMenu = document.querySelector(".navbar__menu");
+const menuDesplegable = document.querySelector(".container-links");
+
+btnMenu.addEventListener('click', desplegarMenu);
+
+function desplegarMenu() {
+    menuDesplegable.classList.toggle("active");
+}
